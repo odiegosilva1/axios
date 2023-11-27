ae3b897e8c2b1f7a72b38392e52d6b51d3f6af63
Diego Silva <odiegosilvadev@gmail.com>
2023-11-27 08:08:26 -0300

    rm: index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function showResponse(res){
-    document.querySelector('#text').innerHTML = `
-        <h1>Informações</h1>
-        <pre>
-            ${JSON.stringify(res.data,null,'\t')}
-        </pre>
-    `
-}
